72de4a280b8869d5876f89605638a529a4fb7105
eXeCUT <sexecut@gmail.com>
2020-10-02 20:34:23 +0300

    Uncommented sitemap module

--- bootstrap/Frontend.php
+++ bootstrap/Frontend.php
@@ -5,6 +5,7 @@
 namespace execut\pages\bootstrap;
 
 
+use assayerpro\sitemap\Sitemap;
 use execut\navigation\Component;
 use execut\navigation\configurator\HomePage;
 use execut\pages\models\Page;
@@ -20,42 +21,42 @@ class Frontend extends Common
     {
         return ArrayHelper::merge(parent::getDefaultDepends(), [
             'bootstrap' => ['pages'],
-//            'modules' => [
-//                'sitemap' => [
-//                    'class' => \assayerpro\sitemap\Module::class,
-//                    'components' => [
-//                        'generator' => [
-//                            'class' => 'assayerpro\sitemap\Generator',
-//                            'models' => [
-//                                // or configuration for creating a behavior
-//                                [
-//                                    'class' => Page::class,
-//                                    'behaviors' => [
-//                                        'sitemap' => [
-//                                            'class' => '\assayerpro\sitemap\behaviors\SitemapBehavior',
-//                                            'scope' => function ($model) {
-//                                                $model->isVisible();
-//                                            },
-//                                            'dataClosure' => function ($model) {
-//                                                /** @var self $model */
-//                                                $date = $model->lastTime;
-//                                                return [
-//                                                    'loc' => $model->getUrl(),
-//                                                    'lastmod' => $date,
-//                                                    'changefreq' => \assayerpro\sitemap\Generator::DAILY,
-//                                                    'priority' => 1
-//                                                ];
-//                                            }
-//                                        ],
-//                                    ],
-//                                ],
-//                            ],
-//                            'enableGzip' => false, // default is false
-//                            'cacheExpire' => 1, // 1 second. Default is 24 hours
-//                        ],
-//                    ],
-//                ],
-//            ],
+            'modules' => [
+                'sitemap' => [
+                    'class' => \assayerpro\sitemap\Module::class,
+                ],
+            ],
+            'components' => [
+                'sitemap' => [
+                    'class' => Sitemap::class,
+                    'models' => [
+                        // or configuration for creating a behavior
+                        [
+                            'class' => Page::class,
+                            'behaviors' => [
+                                'sitemap' => [
+                                    'class' => '\assayerpro\sitemap\behaviors\SitemapBehavior',
+                                    'scope' => function ($model) {
+                                        $model->isVisible();
+                                    },
+                                    'dataClosure' => function ($model) {
+                                        /** @var self $model */
+                                        $date = $model->lastTime;
+                                        return [
+                                            'loc' => $model->getUrl(),
+                                            'lastmod' => $date,
+                                            'changefreq' => \assayerpro\sitemap\Sitemap::DAILY,
+                                            'priority' => 1
+                                        ];
+                                    }
+                                ],
+                            ],
+                        ],
+                    ],
+                    'enableGzip' => false, // default is false
+                    'cacheExpire' => 1, // 1 second. Default is 24 hours
+                ],
+            ],
         ]);
     }
     /**
@@ -67,6 +68,10 @@ class Frontend extends Common
         parent::bootstrap($app);
         $app->getErrorHandler()->errorAction = 'pages/frontend/error';
         $app->urlManager->enablePrettyUrl = true;
+        $app->urlManager->addRules([
+            ['pattern' => 'sitemap-<id:\d+>', 'route' => '/sitemap/default/index', 'suffix' => '.xml'],
+            ['pattern' => 'sitemap', 'route' => 'sitemap/default/index', 'suffix' => '.xml'],
+        ]);
         $navigation = \yii::$app->navigation;
 
         $navigation->addConfigurator([
